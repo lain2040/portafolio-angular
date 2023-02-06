55b37edff9ebd1d1d33eaffadac698f03bf1562e
Clara Ardila <clara.a.ingeniera@gmail.com>
2023-02-06 11:27:29 -0500

    Creación de servicios para la información de el header y footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { InfopaginaService } from './services/infopagina.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portafolio';
+  
+  constructor( public infoPaginaService:InfopaginaService){}
+
 }
---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,13 @@
+
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  pagina_autor?: string;
+  nombre_corto?: string;
+  facebook?: string;
+  instagram?: string;
+  twitter?: string;
+  tumblr?: string;
+  equipo_trabajo?: any[];
+}
+
